9c01bec277d9b49d8a2f965dc5272245fe92ff2c
Will Paceley <pace0033@algonquinlive.com>
2023-04-10 18:42:58 -0400

    Initial Commit

---
+++ VisionTextRecognition/VisionTextRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTextRecognitionApp.swift
+//  VisionTextRecognition
+//
+//  Created by Will Paceley on 2023-04-10.
+//
+
+import SwiftUI
+
+@main
+struct VisionTextRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
