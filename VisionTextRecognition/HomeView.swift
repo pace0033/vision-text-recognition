70202e54c368834527a779f573b95fb35ff0f8f2
Will Paceley <pace0033@algonquinlive.com>
2023-04-10 20:17:38 -0400

    add image processing functionality to HomeView

--- VisionTextRecognition/HomeView.swift
+++ VisionTextRecognition/HomeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Vision
 
 struct HomeView: View {
     @State private var scannedImage: UIImage?
@@ -25,9 +26,13 @@ struct HomeView: View {
             .controlSize(.large)
             .padding()
         }
-        .padding()
         .fullScreenCover(isPresented: $isShowingScanner) {
             ScannerView(scannedImage: $scannedImage)
+        }
+        .onChange(of: scannedImage) { _ in
+            if let image = scannedImage {
+                processImage(image: image)
+            }
         }
     }
 }
@@ -35,5 +40,40 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+extension HomeView {
+    private func processImage(image: UIImage) {
+        guard let cgImage = image.cgImage else {
+            print("Failed to get CGImage from input image")
+            return
+        }
+        
+        // Create a new image-request handler.
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+
+        // Create a new request to recognize text.
+        let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
+
+        do {
+            // Perform the text-recognition request.
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
+    }
+    
+    private func recognizeTextHandler(request: VNRequest, error: Error?) {
+        guard let observations =
+                request.results as? [VNRecognizedTextObservation] else {
+            return
+        }
+        let recognizedStrings = observations.compactMap { observation in
+            // Return the string of the top VNRecognizedText instance.
+            return observation.topCandidates(1).first?.string
+        }
+        
+        print(recognizedStrings)
     }
 }
